8682d0d19445d9ddd069c4d86f98f5b6af52d103
Victor Momoh <momohvictor62@gmail.com>
2023-12-15 13:52:35 +0100

    Date in progress

---
+++ js/main.js
@@ -0,0 +1,52 @@
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+const monthsShort = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const monthYear = document.querySelector("#month-year");
+const date = document.querySelector("#date");
+
+const currentDate = new Date();
+
+const year = currentDate.getFullYear();
+const month = currentDate.getMonth();
+const day = currentDate.getDate();
+const dayOfTheWeek = currentDate.getDay();
+
+monthYear.textContent = `${months[month]} ${year}`;
+date.textContent = `Thursday, Jan 4, ${year}`;
